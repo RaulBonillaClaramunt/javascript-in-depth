3fca5b80e4731c8f4acaa2d1d6ffab412c72e9cb
Raul <raulbonillaclaramunt@email.com>
2024-06-22 19:06:54 +0900

    exercise-1 solved

--- closures-exercises/exercise-1.js
+++ closures-exercises/exercise-1.js
@@ -16,3 +16,21 @@
        text = builder("friend!");
        console.log(text); // "Hello there friend!"
 */
+
+const stringBuilder = () => {
+   let result = '';
+   const add = (characters) => {
+
+      //result += characters;
+      result = result.concat(characters);
+      return result;
+   }
+
+   return add;
+}
+
+const builder = stringBuilder();
+let text = builder("Hello ");
+text = builder("there ");
+text = builder("friend!");
+console.log(text); // "Hello there friend!"
