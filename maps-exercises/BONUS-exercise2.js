1a76fcf2423aeef0bf0f7a2610151a0eb4285b2c
Raul <raulbonillaclaramunt@email.com>
2024-06-27 10:26:22 +0900

    BONUS-exercise2 solved

--- maps-exercises/BONUS-exercise2.js
+++ maps-exercises/BONUS-exercise2.js
@@ -18,7 +18,29 @@
 */
 
 const numbers = [10, 20, 15, 30, 15, 20, 35, 60, 10];
+
 const pairs = new Map();
-for (let i = 0; i < numbers.length - 1; i++) {
+
+for (let i = 0; i < numbers.length; i++) {
+
+   if (pairs.has(numbers[i])) {
+      console.log(i, numbers.indexOf(numbers[i]));
+      break;
+   } else {
+      pairs.set(numbers[i], i);
+   }
+}
+
+console.log(pairs);
+
+/*
+const pairs = new Map();
+for (let i = 0; i < numbers.length; i++) {
    pairs.set(numbers[i], i);
-};
+}; ===> returns Map(6) { 10 => 8, 20 => 5, 15 => 4, 30 => 3, 35 => 6, 60 => 7 }
+for (let i = 0; i < numbers.length; i++) {
+   pairs.set(i, numbers[i]);
+}; ===> returns Map(9) { 0 => 10, 1 => 20, 2 => 15, 3 => 30, 4 => 15, 5 => 20, 6 => 35, 7 => 60, 8 => 10
+}
+console.log(pairs);
+*/
