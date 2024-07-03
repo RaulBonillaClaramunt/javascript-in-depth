754bf5607c98d737db520c1ec8862c9e9a6146a0
Raul <raulbonillaclaramunt@email.com>
2024-07-03 21:09:56 +0900

    warmup exercise solved

--- sets-exercises/warmup-exercise.js
+++ sets-exercises/warmup-exercise.js
@@ -9,3 +9,26 @@
     4. Repeat the same steps above but with an array
        How does this differ?
 */
+let numbers = new Set();
+numbers.add(1);
+numbers.add(2);
+numbers.add(3);
+numbers.add(4);
+numbers.add(5);
+numbers.add(2);
+numbers.add(3);
+numbers.add(4);
+
+//Set has unique elements. If a repeteaded element is added,
+//it won't show
+console.log(numbers);
+let numbers2 = [];
+numbers2.push(1);
+numbers2.push(2);
+numbers2.push(3);
+numbers2.push(4);
+numbers2.push(5);
+numbers2.push(2);
+numbers2.push(3);
+numbers2.push(4);
+console.log(numbers2);
