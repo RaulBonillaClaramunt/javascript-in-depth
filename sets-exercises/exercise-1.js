017d543dd62032f50b15e8b5440cfc3756061d59
Raul <raulbonillaclaramunt@email.com>
2024-07-03 21:13:22 +0900

    exercise-1 solved

--- sets-exercises/exercise-1.js
+++ sets-exercises/exercise-1.js
@@ -4,3 +4,11 @@
 
     2. How can you print out all the UNIQUE points inside "points"?
 */
+const points = [10, 20, 10, 30, 15, 15, 35, 60, 10];
+let unique = new Set();
+
+for (const num of points) {
+    unique.add(num);
+}
+
+console.log(unique);
