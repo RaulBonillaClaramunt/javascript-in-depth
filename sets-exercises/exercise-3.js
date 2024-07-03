28db974cece4bf5bef0bd55c31ac36abaa1468a4
Raul <raulbonillaclaramunt@email.com>
2024-07-04 07:35:05 +0900

    exercise-3 solved

--- sets-exercises/exercise-3.js
+++ sets-exercises/exercise-3.js
@@ -22,13 +22,21 @@
 
     HINT: Use to the Set to keep track of duplicates
 */
-let items = [
+const items = [
     {name: "banana", quantity: 1, price: 1.95},
     {name: "apple", quantity: 1, price: 1.45},
     {name: "banana", quantity: 10, price: 0.05},
     {name: "candy", quantity: 1, price: 3.50}];
 
-let results = [];
+const results = [];
+const duplicates = new Set();
 
-let duplicates = new.Set(items);
-console.log(duplicates);
+for (let i = 0; i < items.length; i++) {
+    if (duplicates.has(items[i].name)) {
+        continue;
+    }
+    results.push(items[i]);
+    duplicates.add(items[i].name);
+}
+
+console.log(results);
