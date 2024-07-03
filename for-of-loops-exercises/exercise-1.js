cc64324d988c86fca212036c30bf9a8543fc3026
Raul <raulbonillaclaramunt@email.com>
2024-07-04 08:16:22 +0900

    exercise-1 solved

--- for-of-loops-exercises/exercise-1.js
+++ for-of-loops-exercises/exercise-1.js
@@ -5,7 +5,7 @@
     2. Create an array called "results" that starts off empty
 
     3. Use a for-of loop to loop over "ages" and add an Object
-       to "results" with an "age" key having the value of the "age" 
+       to "results" with an "age" key having the value of the "age"
        being looped over. Also, add a "name" key that is always "Dragon".
        Eg: {age: 10, name: "Dragon"}, {age: 42, name: "Dragon"}
 
@@ -17,3 +17,13 @@
          {age: 11, name: "Dragon"}, {age: 74, name: "Dragon"},
          {age: 39, name: "Dragon"}, {age: 2, name: "Dragon"}]
 */
+const ages = [10, 42, 15, 22, 11, 74, 39, 2];
+const results = [];
+let object;
+
+for (const age of ages) {
+    const object = { age: age, name: 'Dragon'};
+    results.push(object);
+}
+
+console.log(results);
