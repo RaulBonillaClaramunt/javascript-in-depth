a7d5a036abccd4529ddb5af1dc7d7d9ffdba60e2
Raul <raulbonillaclaramunt@email.com>
2024-07-04 09:59:25 +0900

    exercise-2 solvednode

--- for-of-loops-exercises/exercise-3.js
+++ for-of-loops-exercises/exercise-3.js
@@ -12,3 +12,13 @@
 
     3. Also print out the total value of all items in the backpack
 */
+const backpack = new Map();
+backpack.add('1', {name: "Sword", value: 300});
+backpack.add('2', {name: "Banana", value: 5});
+backpack.add('3', {name: "Gold Nugget", value: 10000});
+backpack.add('4', {name: "Pants", value: 100});
+
+for (const item of backpack) {
+    let nameAndValue = backpack.get(1);
+    console.log(nameAndValue.name + ': ' + nameAndValue.value);
+}
