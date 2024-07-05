9246b36a35db23cbcbf8851f3e38380560b27eb1
Raul <raulbonillaclaramunt@email.com>
2024-07-06 08:27:48 +0900

    Project Black Jack ended ;)

--- Project 1 - Black Jack Game/Project1-Black-Jack-Game.js
+++ Project 1 - Black Jack Game/Project1-Black-Jack-Game.js
@@ -37,11 +37,7 @@ for (const suit of suits) {//deck[0] = {card: x, suit: X}
         deck.push({card: card, suit: suit});
     }
 }
-//console.log(deck);
 
-//randomly pick a card (get a random number between 1 and 52) twice
-//splice each card from deck and push it to playerHand
-//flatten playerHand array (it becomes an array of arrays containing an object)
 let dealerHand = [];
 let dealerScore = 0;
 let playerHand = [];
@@ -51,8 +47,11 @@ let max = 52;
 let gameEnded = false;
 
 for (let i = 0; i < 2; i++) {
+    //randomly pick a card (get a random number between 1 and 52) twice
     let pickaCard = Math.floor(Math.random() * (max - min + 1)) + min;
+    //splice each card from deck and push it to playerHand
     playerHand.push(deck.splice(pickaCard, 1));
+    //flatten playerHand array (it becomes an array of arrays containing an object)
     playerHand = [].concat(...playerHand);
 
     //create a conditional to get player' and dealer' scores
@@ -64,7 +63,7 @@ for (let i = 0; i < 2; i++) {
     } else if (cards.includes(cardValue)) {
     playerScore += Number(cardValue);
     }
-    max--;
+    max--;//each time a card is dealt, max diminishes in one
 }//
 console.log("Starting player hand: ");
 console.log(playerHand);
@@ -94,7 +93,8 @@ console.log('-----');
 
 //player and dealer are dealt 1 more card until score is >= 21.
 for (let index = 0; index < 12; index++) {
-    if (gameEnded) {//if game ending conditions are met, loop breaks and game is finished
+    //if game ending conditions are met, loop breaks and game is finished
+    if (gameEnded) {
         break;
     }
     let pickaCard = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -121,26 +121,18 @@ for (let index = 0; index < 12; index++) {
         gameEnded = true;
         break;
     }
-}
-console.log("Ending player hand: ");
-console.log(playerHand);
-console.log(`Ending player score: ${playerScore}`);
-
-for (let index = 0; index < 12; index++) {
-    if (gameEnded) {
-        break;
-    }
-    let pickaCard = Math.floor(Math.random() * (max - min + 1)) + min;
-    dealerHand.push(deck.splice(pickaCard, 1));
+    //before loop is done, dealer gets a card as well
+    let pickaCard2 = Math.floor(Math.random() * (max - min + 1)) + min;
+    dealerHand.push(deck.splice(pickaCard2, 1));
     dealerHand = [].concat(...dealerHand);
-    let cardValue = dealerHand[dealerHand.length - 1].card;
+    let cardValue2 = dealerHand[dealerHand.length - 1].card;
 
-    if (cardValue === 'Spade' || cardValue === 'Queen' || cardValue === 'King') {
+    if (cardValue2 === 'Spade' || cardValue2 === 'Queen' || cardValue2 === 'King') {
         dealerScore += 10;
-    } else if (cardValue === 'Ace') {
+    } else if (cardValue2 === 'Ace') {
         dealerScore += 1;
-    } else if (cards.includes(cardValue)) {
-        dealerScore += Number(cardValue);
+    } else if (cards.includes(cardValue2)) {
+        dealerScore += Number(cardValue2);
     }
     max--;
     if (dealerScore === 21) {
@@ -155,6 +147,10 @@ for (let index = 0; index < 12; index++) {
         break;
     }
 }
-console.log("Ending dealer hand: ");
+
+console.log("Ending player hand: ");
+console.log(playerHand);
+console.log(`Ending player score: ${playerScore}`);
+console.log(`Ending dealer hand: `);
 console.log(dealerHand);
 console.log(`Ending dealer score: ${dealerScore}`);
