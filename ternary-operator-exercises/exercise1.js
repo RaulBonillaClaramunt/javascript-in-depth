5d9fe7097002897d5b2443403a18910622438776
Raul <raulbonillaclaramunt@email.com>
2024-07-13 07:17:07 +0900

    exercise1 solved

--- ternary-operator-exercises/exercise1.js
+++ ternary-operator-exercises/exercise1.js
@@ -11,3 +11,22 @@
   BONUS: Can you shorten this with the short "arrow function
          expression" syntax?
 */
+const over9000 = (number) => {
+/*
+   if (number > 9000) {
+        return "It's over 9000";
+    } else {
+        return "Taking a Nappa";
+    };
+*/
+    return (number > 9000) ? "It's over 9000" : "Taking a Nappa";
+};
+
+//using the short arrow syntax:
+const over9001 = number => number > 9001 ? "It's over 9001": "Taking a Supernappa";
+
+console.log(over9000(9002));
+console.log(over9000(8002));
+
+console.log(over9001(9002));
+console.log(over9001(8002));
