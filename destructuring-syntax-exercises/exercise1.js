d2da8232a44b5d1f83e704f8fa1261b9a13b53d8
Raul <raulbonillaclaramunt@email.com>
2024-07-13 05:47:22 +0900

    warmup-exercise solved

--- destructuring-syntax-exercises/exercise1.js
+++ destructuring-syntax-exercises/exercise1.js
@@ -19,3 +19,11 @@ const snacks = {
     candy: {name: "Twizzlers", salty: false},
     chocolate: {name: "Mars", salty: false},
 };
+
+
+const { candy, fruit = { name: "Banana", salty: false }, } = snacks;
+
+console.log(candy);
+console.log(fruit);
+
+console.log(snacks.fruit);
