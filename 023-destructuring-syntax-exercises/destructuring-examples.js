26db3d3671b64faa332b18eb03af453453f292b2
Raul <raulbonillaclaramunt@email.com>
2024-07-12 15:14:36 +0900

    directory new arrangement => new numbered names

---
+++ 023-destructuring-syntax-exercises/destructuring-examples.js
@@ -0,0 +1,13 @@
+const cities = ["Vancouver", "Mexico City", "London", "Berlin", "New York"];
+
+const [city1, city2] = cities;
+
+console.log(city1);
+console.log(city2);
+//this is way shorter than declaring variables one by one:
+//const city1 = cities[0];
+//const city2 = cities[1];
+
+const [,, city3,, city5] = cities;
+console.log(city3);
+console.log(city5);
