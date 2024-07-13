d9daab489a1a4d077389623a5ee34cb6b9f8dac4
Raul <raulbonillaclaramunt@email.com>
2024-07-14 08:43:56 +0900

    switch exercises set up

---
+++ 025-switch-exercises/switch-examples.js
@@ -0,0 +1,11 @@
+const animal = 'monkey';
+
+switch (animal) {
+    case "Tiger": {
+        console.log("you are a tigger");
+        break;
+    }
+    case "monkey": {
+        console.log("The best animal");
+    }
+}
