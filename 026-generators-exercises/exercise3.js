9841f4d4cd065340550c1967a61d847c5b4efc7e
Raul <raulbonillaclaramunt@email.com>
2024-07-17 05:08:21 +0900

    exercise3 solved

--- 026-generators-exercises/exercise3.js
+++ 026-generators-exercises/exercise3.js
@@ -32,8 +32,19 @@ const getRandomNumber = function*() {
 
 const groceryList = function*() {
    const groceries = ["Avocado", "Cookie", "Milk", "Soup", "Soda"];
+   //generate a num from 0 to groceries.length
+   for (let i = groceries.length; i >= 0; i--) {
+      const index = Math.floor(Math.random() * groceries.length);
+      const removedItem = groceries.splice(index, 1)[0];
+      yield removedItem;
+   }
 }
 
 const generatorObjectRandom = getRandomNumber();
+const groceriesObject = groceryList();
 
-const generatorObjectGroceries = groceryList();
+for (let i = 0; i < 5; i++) {
+   const randomNumber = generatorObjectRandom.next().value;
+   const groceries = groceriesObject.next().value;
+   console.log(`${randomNumber} ${groceries}`);
+}
